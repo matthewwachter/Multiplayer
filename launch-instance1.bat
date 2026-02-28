f0cc7033e739520f9ec9ada8621905134db20ea1
Matthew Wachter <matthewwachter@gmail.com>
2026-02-28 09:12:01 -0800

    Add launch scripts for multi-instance testing

---
+++ launch-instance1.bat
@@ -0,0 +1,2 @@
+@echo off
+start "" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\RimWorldWin64.exe" --settingsDir "C:\RimWorldMP-Settings\Instance1"
---
+++ launch-instance2.bat
@@ -0,0 +1,2 @@
+@echo off
+start "" "C:\RimWorldMP-Dev\RimWorldWin64.exe" --settingsDir "C:\RimWorldMP-Settings\Instance2"
